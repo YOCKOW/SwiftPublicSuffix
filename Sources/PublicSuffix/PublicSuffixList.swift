30cc31a1520ba4346c9954e5d1f3b20424b8392e
YOCKOW <YOCKOW@users.noreply.github.com>
2020-02-26 23:01:34 +0900

    Update PublicSuffixList.swift with the latest list.

--- Sources/PublicSuffix/PublicSuffixList.swift
+++ Sources/PublicSuffix/PublicSuffixList.swift
@@ -1,5 +1,5 @@
 // This file was created automatically
-//   from https://publicsuffix.org/list/public_suffix_list.dat (Last-Modified: 2020-02-20 16:19:30 UTC)
+//   from https://publicsuffix.org/list/public_suffix_list.dat (Last-Modified: 2020-02-25 22:53:29 UTC)
 
 // NOTICE: Original source code is licensed under Mozilla Public License Version 2.0 (MPL2.0)
 //         and, this file contains the source converted to Swift language.
